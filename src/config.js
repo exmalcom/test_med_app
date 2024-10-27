f557e21cd2a5cc588f25f06d97cf6d7a1c1cdde1
exmalcom <htcteambangis@gmail.com>
2024-10-27 10:26:04 -0400

    I put the links of the server-side

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = window.location.hostname === "localhost" ? "<add your theia server side url>" : "add your theia server side url";
+export const API_URL = window.location.hostname === "localhost" ? "https://htcteambangi-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://htcteambangi-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";   //Note: Make sure to replace the URL both before and after the colon(:) to avoid compile time errors. THAT LINKS USED ARE SERVER-SIDE LINKS
 console.log(
     "API_URL :",
     API_URL
