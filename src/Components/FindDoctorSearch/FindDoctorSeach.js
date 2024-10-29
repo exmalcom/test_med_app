e14c4fe0a4e077d72a25cc4b9efc40a081b42c08
Jerald Calunsag <htcteambangis@gmail.com>
2024-10-29 18:30:26 +0800

    Fixed the path for consultation

--- src/Components/FindDoctorSearch/FindDoctorSeach.js
+++ src/Components/FindDoctorSearch/FindDoctorSeach.js
@@ -15,7 +15,7 @@ const FindDoctorSearch = () => {
     const handleDoctorSelect = (speciality) => {
         setSearchDoctor(speciality);
         setDoctorResultHidden(true);
-        navigate(`/instant-consultation?speciality=${speciality}`);
+        navigate(`/consultation?speciality=${speciality}`);
         window.location.reload();
     }
     return (
