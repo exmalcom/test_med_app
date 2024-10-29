5649cb1cceb54eb035af4d003087874a500f7bb8
Jerald Calunsag <htcteambangis@gmail.com>
2024-10-29 18:27:18 +0800

    Change if you want to work the MongoDB in your VS Code application

--- src/config.js
+++ src/config.js
@@ -1,5 +1,2 @@
-export const API_URL = window.location.hostname === "localhost" ? "https://htcteambangi-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://htcteambangi-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";   //Note: Make sure to replace the URL both before and after the colon(:) to avoid compile time errors. THAT LINKS USED ARE SERVER-SIDE LINKS
-console.log(
-    "API_URL :",
-    API_URL
-);
+export const API_URL = "http://localhost:8181";   //Note: Make sure to replace the URL both before and after the colon(:) to avoid compile time errors. THAT LINKS USED ARE SERVER-SIDE LINKS
+console.log("API_URL :", API_URL ); // Log the API URL for debugging
